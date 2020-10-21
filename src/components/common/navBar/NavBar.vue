318904b6378a6c05aaacfd03327a8090084ac863
cxk <1098154715@qqq.com>
2020-10-21 19:46:53 +0800

    补充第一次提交

---
+++ src/components/common/navBar/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="nav-bar">
+		<!--添加插槽，上面不用id是因为很多地方要用
+		  下面用div是因为插槽不不能设置样式-->
+		<div class="left">
+			<slot name="left"></slot>
+		</div>
+		<div class="center">
+			<slot name="center"></slot>
+		</div>
+		<div class="right">
+			<slot name="left"></slot>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "NavBar"
+	}
+</script>
+
+<style>
+	.nav-bar {
+		display: flex;
+		line-height: 44px;
+		height: 44px;
+		text-align: center;
+	}
+
+	.left,
+	.right {
+		width: 60px;
+	}
+
+	.center {
+		flex: 1;
+		color: white;
+	}
+</style>
--- src/components/views/home/homeindex.vue
+++ src/components/views/home/homeindex.vue
@@ -1,9 +1,23 @@
 <template>
-	<h2>我是主页</h2>
+	<div id="home">
+		<nav-bar class="home-bar-background" >
+			<div slot="center">购物车</div>
+		</nav-bar>
+	</div>
 </template>
 
 <script>
+	import NavBar from "components/common/navBar/NavBar.vue"
+	export default {
+		name: "homeindex",
+		components: {
+			NavBar
+		}
+	}
 </script>
 
 <style>
+	.home-bar-background{
+		background-color: #5FBD45;
+	}
 </style>
